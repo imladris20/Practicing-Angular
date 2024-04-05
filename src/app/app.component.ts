69084902d941c582a11de3376ea6abcee6482368
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-05 11:19:03 +0800

    Protice @if, @else by adding server-status component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ServerStatusComponent } from './server-status/server-status.component';
 import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, UserComponent],
+  imports: [RouterOutlet, UserComponent, ServerStatusComponent],
   template: `
     <section>
       <app-user></app-user>
+    </section>
+    <section>
+      <app-server-status></app-server-status>
     </section>
     <p>Welcome to {{ city }}! Now is year {{ 2023 + 1 }}</p>
   `,
---
+++ src/app/server-status/server-status.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server-status',
+  standalone: true,
+  imports: [],
+  template: `
+    @if(isServerRunning) {
+    <p>The server is running.</p>
+    } @else {
+    <p>The server is not running</p>
+    }
+  `,
+  styleUrl: './server-status.component.css',
+})
+export class ServerStatusComponent {
+  isServerRunning: boolean = true;
+}
