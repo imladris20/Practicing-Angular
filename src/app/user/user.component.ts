79011952fcd69baf567cb9ea0851b30da8261224
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-05 11:40:17 +0800

    Practice @for syntax

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,9 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-user',
   standalone: true,
   imports: [],
-  template: ` <p>Username: {{ username }}</p> `,
+  template: `
+    <p>mainUser: {{ mainUser }}</p>
+    <h1>Other Users</h1>
+    @for(user of users ; track user.id){
+    <h3>{{ user.name }}</h3>
+    }
+  `,
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  username = 'Po Lien, aspiring programmer';
+  mainUser = 'Po Lien, aspiring programmer';
+  users = [
+    { id: 0, name: 'Sarah' },
+    { id: 1, name: 'Amy' },
+    { id: 2, name: 'Rachel' },
+    { id: 3, name: 'Jessica' },
+    { id: 4, name: 'Poornima' },
+  ];
 }
