a5ffa9a1acf0c31d3cfe46bd3a157803276a0a7e
Po Lien Lin <polien.joe.lin@gmail.com>
2024-04-05 10:36:49 +0800

    Practice adding and using new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `Welcome to {{ city }}! Now is year {{ 2023 + 1 }}`,
+  imports: [RouterOutlet, UserComponent],
+  template: `
+    <section>
+      <app-user></app-user>
+    </section>
+    <p>Welcome to {{ city }}! Now is year {{ 2023 + 1 }}</p>
+  `,
   styles: `
     :host{
       color: #a144eb;
